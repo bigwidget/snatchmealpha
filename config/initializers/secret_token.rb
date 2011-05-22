5e3585b1f71026dbdb03b0351c663814ef6a2e81
Tim Koelkebeck <tim@mytype.com>
2011-05-22 12:09:08 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snatchmealpha::Application.config.secret_token = '5c21e2c338f464b8f4e83b103ddeb63133255e87e4c78e94ced484b0ae4a29e785717a8e1533f1ad24e1bc5f21461ff07502ea263626144f7b9f9309908a548a'
